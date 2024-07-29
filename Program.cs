8b95b0685e4af891d0bbe34e7b19dcd47071a922
vagiz <ahatovvagiz@gmail.com>
2024-07-29 21:33:20 +0300

    lesson2 commit7

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+<<<<<<< HEAD
 Console.WriteLine("Hello, commit 4!");
 Console.WriteLine("Hello, commit 5!");
 Console.WriteLine("Hello, commit 6!");
+=======
+>>>>>>> parent of 637fe5c (lesson1 commit4)
