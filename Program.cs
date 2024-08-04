11b9b9ad233e715de51aa9ce8f9462d7943fac4b
Azat <user@WIN-Q595KQ6H928>
2024-08-04 22:03:24 +0300

    lesson3_1

--- Program.cs
+++ Program.cs
@@ -4,5 +4,7 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, commit 4!");
 Console.WriteLine("Hello, commit 5!");
 Console.WriteLine("Hello, commit 6!");
+
+Console.WriteLine("Hello, commit lesson3_1!");
 =======
 >>>>>>> parent of 637fe5c (lesson1 commit4)
